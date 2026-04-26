44003799b47d8547e5366bf49d649913c081a7f2
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-04-26 19:25:41 +0800

    implement ProfileToolbar

--- src/components/profile/ProfileToolbar.vue
+++ src/components/profile/ProfileToolbar.vue
@@ -1,4 +1,25 @@
 <template>
-  <div data-test="stub" />
+  <v-toolbar density="compact" flat class="profile-toolbar">
+    <v-toolbar-title>{{ tv('profiles.title', 'Profiles') }}</v-toolbar-title>
+    <v-spacer />
+    <v-btn
+      icon="mdi-refresh"
+      :title="tv('common.reload', 'Reload')"
+      data-test="profile-refresh"
+      :disabled="!store.sourceKey || !store.personId"
+      @click="onReload"
+    />
+  </v-toolbar>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { useProfileStore } from '@/stores/profile'
+import { useI18n } from '@/composables/useI18n'
+
+const store = useProfileStore()
+const { tv } = useI18n()
+
+async function onReload() {
+  await store.loadPerson()
+}
+</script>
