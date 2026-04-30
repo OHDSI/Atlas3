b4b91d5b56b996c18ff93f0bea2110147bab95eb
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-04-30 14:44:56 +0800

    refactor(design): migrate CohortBuilderView outer shell to PageShell

--- src/views/CohortBuilderView.vue
+++ src/views/CohortBuilderView.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="page-wrapper">
-    <div class="page-card">
-      <cohort-builder :id="id" />
-    </div>
-  </div>
+  <page-shell>
+    <cohort-builder :id="id" />
+  </page-shell>
 </template>
 
 <script setup lang="ts">
+import PageShell from '@/components/shared/PageShell.vue'
 import CohortBuilder from '@/components/cohort/CohortBuilder.vue'
 
 /**
@@ -17,21 +16,3 @@ defineProps<{
   id?: string
 }>()
 </script>
-
-<style scoped>
-.page-wrapper {
-  min-height: 100%;
-  background-color: rgb(var(--v-theme-background));
-  display: flex;
-  padding: 32px;
-  box-sizing: border-box;
-}
-
-.page-card {
-  border-radius: 18px;
-  padding: 30px;
-  background-color: white;
-  width: 100%;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
-}
-</style>
